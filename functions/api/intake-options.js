46ef8abbf050414717856c195300e1b50da58bd8
Team IOTC <iotcsupport@gmail.com>
2026-09-20 07:23:51 +0530

    Only offer wards with a reachable rep in manual ward search

--- functions/api/intake-options.js
+++ functions/api/intake-options.js
@@ -10,7 +10,7 @@ export async function onRequestGet({ env }) {
     ).all();
 
     const unitsResult = await env.DB.prepare(
-      `SELECT id, name, unit_type, localities FROM local_units ORDER BY name ASC`
+      `SELECT id, name, unit_type, localities FROM local_units WHERE rep_email IS NOT NULL ORDER BY name ASC`
     ).all();
 
     return new Response(
